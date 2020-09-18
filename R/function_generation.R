dd61f9333152a5895cdba162674127d5662e368b
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-09-18 07:47:09 +0300

    doc fixes based on R CMD CHECK

--- R/function_generation.R
+++ R/function_generation.R
@@ -42,6 +42,12 @@ NULL
 #' this environment will be the parent environment of each of the temporary
 #' environments; the default is the parent environment of the function execution
 #' environment
+
+#' @param call `[language, NULL]` (optional, default `NULL`)
+#'
+#' - `language`: an R language object such as one produced by [match.call] or
+#'   `quote`; this call will be reported in an error
+#' - `NULL`: the call is attempted to be inferred
 #'
 #' @section Interpolation in messages:
 #' `fail_messages` and `pass_messages` can use variables you create in a test
--- R/manually_written_check_funs.R
+++ R/manually_written_check_funs.R
@@ -14,6 +14,11 @@
 #' the name of the object `x` to mention in possible error message.
 #' - `NULL`: taken as `deparse(substitute(x))`
 #' - `character`: the name as a string
+#' @param call `[language, NULL]` (optional, default `NULL`)
+#'
+#' - `language`: an R language object such as one produced by [match.call] or
+#'   `quote`; this call will be reported in an error
+#' - `NULL`: the call is attempted to be inferred
 #' @param lo `[number]` (mandatory, no default)
 #' lower bound for `x`
 #'
