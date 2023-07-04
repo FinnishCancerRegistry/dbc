71a5b437732e8c4b058f0bc86de519b56ded5b01
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-07-04 12:14:44 +0300

    test: update test_call.R

--- tests/testthat/test_call.R
+++ tests/testthat/test_call.R
@@ -1,30 +1,19 @@
-
-
-
-
-
 testthat::test_that("assert_* funs show correct call in error msg", {
-
   tf <- function(arg) {
-
     dbc::assert_user_input_is_character_nonNA_atom(arg)
-
   }
-
   err <- tryCatch(tf(1L), error = function(e) e)
-
-  testthat::expect_true(grepl("\\Qtf(arg = 1L)\\E", err$message))
   testthat::expect_true(
-    grepl("assert_user_input_is_character_nonNA_atom", deparse(err$call))
+    grepl("\\Qtf(arg = 1L)\\E", err$message)
+  )
+  testthat::expect_true(
+    grepl("\\Qtf(arg = 1L)\\E", deparse(err$call))
   )
 
 })
 
-
 testthat::test_that("report_to_assertion shows correct call in error msg", {
-
   tf <- function(arg) {
-
     dbc::report_to_assertion(
       dbc::expressions_to_report(
         expressions = "is.character(arg)"
@@ -32,13 +21,7 @@ testthat::test_that("report_to_assertion shows correct call in error msg", {
     )
 
   }
-
   err <- tryCatch(tf(1L), error = function(e) e)
-
   testthat::expect_true(grepl("\\Qtf(arg = 1L)\\E", err$message))
   testthat::expect_true(grepl("\\Qtf(arg = 1L)\\E", deparse(err$call)))
-
 })
-
-
-
