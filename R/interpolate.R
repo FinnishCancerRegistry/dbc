7a9f6daacc59502550d282e0a147f656b1189575
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-12-29 09:47:02 +0200

    refactor: optimise interpolate a bit

--- R/interpolate.R
+++ R/interpolate.R
@@ -3,6 +3,7 @@
 interpolate <- function(x, env = parent.frame(1L)) {
   stopifnot(
     is.character(x),
+    length(x) == 1,
     is.environment(env)
   )
   # @codedoc_comment_block dbc:::interpolate
@@ -17,13 +18,12 @@ interpolate <- function(x, env = parent.frame(1L)) {
   #    will not be parsed correctly.
   # @codedoc_comment_block dbc:::interpolate
   m <- gregexpr(pattern = "[$][{][^{]+[}]", text = x, perl = TRUE)
-  expr_strings_by_x_elem <- regmatches(x = x, m = m)
-  has_nothing_to_interpolate <- length(expr_strings_by_x_elem) == 1L &&
-    length(expr_strings_by_x_elem[[1L]]) == 0L
+  has_nothing_to_interpolate <- length(m) == 1L && identical(m[[1]], -1L)
   if (has_nothing_to_interpolate) {
     return(x)
   }
 
+  expr_strings_by_x_elem <- regmatches(x = x, m = m)
   values <- lapply(expr_strings_by_x_elem, function(expr_string_vec) {
     # @codedoc_comment_block dbc:::interpolate
     # 2. Each expression substring is evaluated in the
