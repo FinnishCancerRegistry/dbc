32be551f19ec344273a08391a2931ca54774376e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2024-01-16 09:27:23 +0200

    build: improve data-raw/function_chunks

--- data-raw/function_chunks/body_start.R
+++ data-raw/function_chunks/body_start.R
@@ -4,14 +4,4 @@
 # where `x` was attempted to be evaluated only in a call to `eval` which
 # resulted in cryptic error messages --- now those can no longer occur.
 # @codedoc_comment_block news("dbc", "2023-08-16", "0.4.16")
-x_nm <- dbc::handle_arg_x_nm(x_nm)
-call <- dbc::handle_arg_call(call)
-if (missing(x)) {
-  stop(simpleError(
-    message = paste0(
-      "Argument `", x_nm, "` was missing --- it has no default so ",
-      "some value must be supplied!"
-    ),
-    call = call
-  ))
-}
+dbc::handle_args_inplace()
