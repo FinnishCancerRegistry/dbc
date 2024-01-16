562d6959d91c5299182ce76f19d1795bd9e1af5e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2024-01-16 11:01:55 +0200

    test: update test_call.R

--- tests/testthat/test_call.R
+++ tests/testthat/test_call.R
@@ -2,14 +2,44 @@ testthat::test_that("assert_* funs show correct call in error msg", {
   tf <- function(arg) {
     dbc::assert_user_input_is_character_nonNA_atom(arg)
   }
-  err <- tryCatch(tf(1L), error = function(e) e)
-  testthat::expect_true(
-    grepl("\\Qtf(arg = 1L)\\E", err$message)
+  err <- tryCatch(
+    tf(1L),
+    error = function(e) e
   )
-  testthat::expect_true(
-    grepl("\\Qtf(arg = 1L)\\E", deparse(err$call))
+  testthat::expect_identical(
+    "tf(arg = 1L)",
+    deparse1(err$call)
+  )
+  
+  err <- tryCatch(
+    dbc::assert_user_input_is_character_nonNA_atom(1L),
+    error = function(e) e
+  )
+  testthat::expect_identical(
+    "dbc::assert_user_input_is_character_nonNA_atom(x = 1L)",
+    deparse1(err$call)
   )
 
+  tf <- function(arg) {
+    dbc::assert_is_like_template(arg, template = 1L)
+  }
+  err <- tryCatch(
+    tf("yo"),
+    error = function(e) e
+  )
+  testthat::expect_identical(
+    "tf(arg = \"yo\")",
+    deparse1(err$call)
+  )
+  
+  err <- tryCatch(
+    dbc::assert_is_like_template(x = "yo", template = 1L),
+    error = function(e) e
+  )
+  testthat::expect_identical(
+    "dbc::assert_is_like_template(x = \"yo\", template = 1L)",
+    deparse1(err$call)
+  )
 })
 
 testthat::test_that("report_to_assertion shows correct call in error msg", {
@@ -22,6 +52,5 @@ testthat::test_that("report_to_assertion shows correct call in error msg", {
 
   }
   err <- tryCatch(tf(1L), error = function(e) e)
-  testthat::expect_true(grepl("\\Qtf(arg = 1L)\\E", err$message))
   testthat::expect_true(grepl("\\Qtf(arg = 1L)\\E", deparse(err$call)))
 })
